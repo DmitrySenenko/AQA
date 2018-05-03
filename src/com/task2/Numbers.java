2f30c3dd51a1f5221c0349fc6a16210dde90db42
Dmitry <dmitry@cryptonomos.com>
2018-05-03 20:49:02 +0300

    PC

--- src/com/task2/Numbers.java
+++ src/com/task2/Numbers.java
@@ -13,10 +13,10 @@ public class Numbers {
             int sc = scanner.nextInt();
             int[] numb = new int[sc];
 
-            int max = 0;
+            int sum = 0;
 
 
-            System.out.println("Введите ващи числа, каждое последующее через Enter!");
+            System.out.println("Введите ващи числа, каждое последующее через Enter(");
             for (int i = 0; i < numb.length; i++) {
                 numb[i] = scanner.nextInt();
             }
@@ -24,11 +24,11 @@ public class Numbers {
             for (int i = numb.length - 1; i >= 0; i--) {
                 for (int j = 0; j < i; j++) {
                     String len = Integer.toString(numb[j]);
-                    String len1 = Integer.toString(numb[j+1]);
+                    String len1 = Integer.toString(numb[j + 1]);
                     if (len.length() > len1.length()) {
                         int tmp = numb[j];
-                        numb[j] = numb[j+1];
-                        numb[j+1] = tmp;
+                        numb[j] = numb[j + 1];
+                        numb[j + 1] = tmp;
                     }
                 }
             }
@@ -60,13 +60,79 @@ public class Numbers {
             System.out.println("Обратный порядок массива: " + Arrays.toString(numb));
 
             for (int i = 0; i < numb.length; i++) {
-                //todo узнать среднее значение в массиве
+                String len = Integer.toString(numb[i]);
+                sum += len.length();
             }
-            System.out.println("Среднее кол-во символов массива " + max);
+
+
+            System.out.println("Сумма массива: " + sum);
+            int polusum = sum / sc;
+            System.out.println("Полусумма массива: " + polusum);
+
+            for (int i = 0; i < numb.length; i++) {
+                String len = Integer.toString(numb[i]);
+                if (len.length() >= polusum) {
+                    System.out.println("Кол-во символов больше(или равно) полусумме:" + numb[i]);
+                }
+            }
+
+            for (int i = 0; i < numb.length; i++) {
+                String len = Integer.toString(numb[i]);
+                if (len.length() < polusum) {
+                    System.out.println("Кол-во символов меньше полусуммы:" + numb[i]);
+                }
+            }
+
+            for (int i = 0; i < numb.length; i++) {
+                for (numb[i] >= 10 || numb[i] < 99 || numb[i] <= -10 || numb[i] > -99) {
+                    int n1 = numb[i] % 10;
+                    int n2 = numb[i] / 10 % 10;
+                    if (n1 != n2) {
+                        System.out.println("Одинаковых цифр нет в числе: " + " " + numb[i]);
+                    }
+                    break;
+                }for (numb[i] >= 100 && numb[i] < 100 && numb[i] <= -100 && numb[i] > -999) {
+                    int n1 = numb[i] % 10;
+                    int n2 = numb[i] / 10 % 10;
+                    int n3 = numb[i] / 100 % 10;
+                    if (n1 != n2 && n1 != n3 && n2 != n3) {
+                        System.out.println("Одинаковых цифр нет в числе: " + " " + numb[i]);
+
+                    }
+                    break;
+                } for (numb[i] >= 1000 && numb[i] < 9999 && numb[i] <= -1000 && numb[i] > -9999) {
+                    int n1 = numb[i] % 10;
+                    int n2 = numb[i] / 10 % 10;
+                    int n3 = numb[i] / 100 % 10;
+                    int n4 = numb[i] / 1000 % 10;
+                    if (n1 != n2 && n1 != n3 && n1 != n4 && n2 !=n3 && n2 != n4 && n3 !=n4) {
+                        System.out.println("Одинаковых цифр нет в числе: " + " " + numb[i]);
+
+                    }break;
+
+                }for (numb[i] >= 10000 && numb[i] < 99999 && numb[i] <= -10000 && numb[i] > -99999) {
+                    int n1 = numb[i] % 10;
+                    int n2 = numb[i] / 10 % 10;
+                    int n3 = numb[i] / 100 % 10;
+                    int n4 = numb[i] / 1000 % 10;
+                    int n5 = numb[i] / 10000 % 10;
+                    if (n1 != n2 && n1 != n3 && n1 != n4 && n1 !=n5 && n2 != n3 && n2 !=n4 && n2 != n5 && n3 != n4 && n3 != n5 && n4 !=n5 ) {
+                        System.out.println("Одинаковых цифр нет в числе: " + " " + numb[i]);
+
+                    }
+                    break;
+
+                }
+            }
+
+            System.out.println("Массив слева направо:");
 
             for (int i = numb.length - 1; i >= 0; i--) {
                 System.out.print(numb[i] + " ");
             }
+
+            System.out.println(" ");
+            System.out.println("Массив сверху вниз:");
 
             for (int i = numb.length - 1; i >= 0; i--) {
                 System.out.println(numb[i] + " ");
@@ -74,8 +140,12 @@ public class Numbers {
 
 
             scanner.close();
-        } catch (InputMismatchException err) {
+        }catch (InputMismatchException err) {
             System.out.println("Вы ввели буквы, а нужно цифры");
+        }catch (NegativeArraySizeException err) {
+            System.out.println("Массив не может иметь отрицательную длинну");
+        }catch (ArrayIndexOutOfBoundsException err) {
+            System.out.println("Массив не может иметь нулевую длину");
         }
     }
 }
