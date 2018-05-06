94e51972c8ba70dc9c5dc87d3dd158ed838b48ba
DmitrySenenko <dmitry.senenko@gmail.com>
2018-05-06 09:40:27 +0300

    note

--- src/Lesson_4.java
+++ src/Lesson_4.java
@@ -1,7 +1,19 @@
+import javax.swing.*;
+
 public class Lesson_4 {
 
     public static void main(String[] args) {
+        Girl Ira;
+        //System.out.println(Ira);
+        System.out.println(new Girl());
+
         new Girl().goToCinema();
+
+
+
+
+
+
 
     }
 }
