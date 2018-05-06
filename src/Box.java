cfbdc3559a3b7966923b3f48fac6ecb7b476848c
DmitrySenenko <dmitry.senenko@gmail.com>
2018-05-06 09:55:53 +0300

    note

--- src/Box.java
+++ src/Box.java
@@ -1,21 +1,47 @@
 
-public class Box {
+class Box {
     double width;
     double height;
     double depth;
 
+    double volume() {
+        return height*width*depth;
+    }
 
-    static class BoxDemo {
-        public static void main(String[] args) {
+}
+
+
+class BoxDemo {
+        /*public static void main(String[] args) {
             Box mybox = new Box();
             double vol;
             mybox.width = 10;
             mybox.height = 20;
             mybox.depth = 15;
 
-            vol = mybox.width * mybox.height * mybox.depth;
-            System.out.println("Объем равен: " + vol);
+           vol = mybox.width * mybox.height * mybox.depth;
+            System.out.println("Объем равен: " + vol);*/
 
+
+    static class BoxDemo3 {
+        public static void main(String[] args) {
+            Box mybox1 = new Box();
+            Box mybox2 = new Box();
+
+            mybox1.width = 10;
+            mybox1.height = 20;
+            mybox1.depth = 15;
+
+            mybox2.width = 3;
+            mybox2.height = 6;
+            mybox2.depth = 9;
+
+
+            double vol = mybox1.volume();
+            System.out.println("Volume is " + vol);
+
+            double vol2= mybox2.volume();
+            System.out.println("Volume is " + vol2);
         }
     }
 }
