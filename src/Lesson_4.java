0f60ecb9e91baedce6b3f62ea921477ef01ee673
DmitrySenenko <dmitry.senenko@gmail.com>
2018-05-06 10:19:16 +0300

    note

--- src/Lesson_4.java
+++ src/Lesson_4.java
@@ -1,19 +1,23 @@
-import javax.swing.*;
+
 
 public class Lesson_4 {
+    static double avg(int... a) {
+        double sum = 0;
+        for (int i : a) {
+            sum += i;
+        }
+        return sum / a.length;
+    }
+
 
     public static void main(String[] args) {
-        Girl Ira;
+        //Girl Ira;
         //System.out.println(Ira);
-        System.out.println(new Girl());
+        //System.out.println(new Girl());
 
-        new Girl().goToCinema();
-
-
-
-
-
-
+        //new Girl().goToCinema();
+        System.out.println(avg( 1,2,3,4,5,6,7,8,9));
+        System.out.println(avg(2,2,2,2,2,2,2,2,2));
 
     }
 }
