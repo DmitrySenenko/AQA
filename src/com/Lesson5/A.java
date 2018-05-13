7386b6d1ed89d07c54f7092d8c98b7aaa2ab7ec3
DmitrySenenko <dmitry.senenko@gmail.com>
2018-05-13 10:34:31 +0300

    note

---
+++ src/com/Lesson5/A.java
@@ -0,0 +1,19 @@
+package com.Lesson5;
+
+public class A {
+    int a;
+    int b;
+
+    public A(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public String toString() {
+        return "A{" +
+                "a=" + a +
+                ", b=" + b +
+                '}';
+    }
+}
---
+++ src/com/Lesson5/Main.java
@@ -0,0 +1,9 @@
+package com.Lesson5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        A a = new A(11, 23);
+        System.out.println(a.toString());
+    }
+}
