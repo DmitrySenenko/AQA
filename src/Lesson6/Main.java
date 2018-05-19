adcae9ea6ae23cdb017069a469485c5cf3b71d79
DmitrySenenko <dmitry.senenko@gmail.com>
2018-05-19 14:26:59 +0300

    note

--- src/Lesson6/Main.java
+++ src/Lesson6/Main.java
@@ -1,6 +1,7 @@
 package Lesson6;
 
 import java.util.Arrays;
+
 import Lesson6.Ball;
 
 public class Main {
@@ -33,12 +34,20 @@ public class Main {
         System.out.println(a.b);*/
 
         //B.m1();
-        B b = new B();
+        /*B b = new B();
         A a = new A();
-        A a1 = new A();
+        A a1 = new A();*/
         //System.out.println(B.b);  //100
         //System.out.println(A.b);  //0
 
+        Shape s[] = new Shape[3];
+        s[0] = new Circle("Black", 10, 10, 5);
+        s[1] = new Rectangle("yellow", 10, 10, 50, 50);
+        s[2] = new Circle("green", 10, 10, 5);
+
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(s[i].draw());
+        }
 
     }
 }
