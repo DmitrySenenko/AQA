51085abefbb3aef264007cdb95190fdfcfc55c55
DmitrySenenko <dmitry.senenko@gmail.com>
2018-05-19 14:21:02 +0300

    note

---
+++ src/Lesson6/Circle.java
@@ -0,0 +1,18 @@
+package Lesson6;
+
+public class Circle extends Shape {
+
+    int x,y,z;
+
+    public Circle(String color, int x, int y, int z) {
+        super(color);
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Override
+    public String draw() {
+        return "circle";
+    }
+}
---
+++ src/Lesson6/Rectangle.java
@@ -0,0 +1,19 @@
+package Lesson6;
+
+public class Rectangle extends Shape {
+
+    protected int x,y,x1,y1;
+
+    public Rectangle(String color, int x, int y, int x1, int y1) {
+        super(color);
+        this.x = x;
+        this.y = y;
+        this.x1 = x1;
+        this.y1 = y1;
+    }
+
+    @Override
+    public String draw() {
+        return "Rectangle";
+    }
+}
