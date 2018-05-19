d181532389f627ce1cda22cbcf964df606474511
DmitrySenenko <dmitry.senenko@gmail.com>
2018-05-19 12:32:52 +0300

    note

---
+++ src/Lesson6/B.java
@@ -0,0 +1,8 @@
+package Lesson6;
+
+public class B extends A{
+    @Override
+    void m() {
+        System.out.println("B");
+    }
+}
---
+++ src/Lesson6/Main.java
@@ -0,0 +1,13 @@
+package Lesson6;
+
+public class Main {
+
+    public static void main(String[] args) {
+        A a = new A();
+        a.m();
+        a = new C();
+        a.m();
+        a = new B();
+        a.m();
+    }
+}
