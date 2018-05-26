08e034026433a482cd6670876518bb36f8359441
DmitrySenenko <dmitry.senenko@gmail.com>
2018-05-26 14:09:42 +0300

    note

--- src/labs/Lesson7/EnumDemo.java
+++ src/labs/Lesson7/EnumDemo.java
@@ -2,7 +2,7 @@ package labs.Lesson7;
 
 public class EnumDemo {
     public static void main(String[] args) {
-        Apple ap;
+        Apple ap, ap2, ap3;
 
         /*ap=Apple.RedDel;
 
@@ -23,13 +23,38 @@ public class EnumDemo {
 
         System.out.println("Here are all Apple constants");
 
-        Apple allapples[]= Apple.values();
-        for(Apple a: allapples)
+        Apple allapples[] = Apple.values();
+        for (Apple a : allapples)
             System.out.println(a);
 
         System.out.println();
 
-        ap=Apple.valueOf("Winesap");
+        for (Apple a : Apple.values())
+            System.out.println(a + " " + a.ordinal()); //Позиция константы в списке
+
+        ap = Apple.RedDel;
+        ap2 = Apple.GoldenDel;
+        ap3 = Apple.RedDel;
+
+        if (ap.compareTo(ap2) < 0)
+            System.out.println(ap + " comes before " + ap2);
+
+        if (ap.compareTo(ap2) > 0)
+            System.out.println(ap2 + " comes before " + ap);
+
+        if (ap.compareTo(ap3) == 0)
+            System.out.println(ap + " equals " + ap3);
+
+        System.out.println();
+
+        if (ap.equals(ap2))
+            System.out.println("Error!");
+        if (ap.equals(ap3))
+            System.out.println(ap+ " equals " + ap3);
+        if (ap==ap3)
+            System.out.println(ap + " == " + ap3);
+
+        ap = Apple.valueOf("Winesap");
         System.out.println("ap contains " + ap);
     }
 }
