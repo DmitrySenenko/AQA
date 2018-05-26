6b1c5efc339edd793be11fa022333f7660f1f355
DmitrySenenko <dmitry.senenko@gmail.com>
2018-05-26 13:50:08 +0300

    note

--- src/labs/Lesson7/EnumDemo.java
+++ src/labs/Lesson7/EnumDemo.java
@@ -4,7 +4,7 @@ public class EnumDemo {
     public static void main(String[] args) {
         Apple ap;
 
-        ap=Apple.RedDel;
+        /*ap=Apple.RedDel;
 
         System.out.println("Value of ap: " + ap);
         System.out.println();
@@ -19,5 +19,17 @@ public class EnumDemo {
                 System.out.println("Jonathan is red");
                 break;
         }
+        */
+
+        System.out.println("Here are all Apple constants");
+
+        Apple allapples[]= Apple.values();
+        for(Apple a: allapples)
+            System.out.println(a);
+
+        System.out.println();
+
+        ap=Apple.valueOf("Winesap");
+        System.out.println("ap contains " + ap);
     }
 }
